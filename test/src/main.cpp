9210754a20eb98b5e9f7e6362fd4ee3e6cc444f4
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2022-09-26 21:26:41 -0400

    brief testing of SPIFFS and libssh
    it works so far

---
+++ test/src/main.cpp
@@ -0,0 +1,140 @@
+#include <Arduino.h>
+#include <WiFi.h>
+
+#include "FS.h"
+#include "SPIFFS.h"
+#include "libssh_esp32.h"
+
+#define FORMAT_SPIFFS_IF_FAILED false
+
+const char *ssid = ***REMOVED***;
+const char *password = ***REMOVED***;
+
+void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
+    Serial.printf("Listing directory: %s\r\n", dirname);
+
+    File root = fs.open(dirname);
+    if (!root) {
+        Serial.println("− failed to open directory");
+        return;
+    }
+    if (!root.isDirectory()) {
+        Serial.println(" − not a directory");
+        return;
+    }
+
+    File file = root.openNextFile();
+    while (file) {
+        if (file.isDirectory()) {
+            Serial.print("  DIR : ");
+            Serial.println(file.name());
+            if (levels) {
+                listDir(fs, file.name(), levels - 1);
+            }
+        } else {
+            Serial.print("  FILE: ");
+            Serial.print(file.name());
+            Serial.print("\tSIZE: ");
+            Serial.println(file.size());
+        }
+        file = root.openNextFile();
+    }
+}
+
+void readFile(fs::FS &fs, const char *path) {
+    Serial.printf("Reading file: %s\r\n", path);
+
+    File file = fs.open(path);
+    if (!file || file.isDirectory()) {
+        Serial.println("− failed to open file for reading");
+        return;
+    }
+
+    Serial.println("− read from file:");
+    while (file.available()) {
+        Serial.write(file.read());
+    }
+}
+
+void writeFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if (!file) {
+        Serial.println("− failed to open file for writing");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("− file written");
+    } else {
+        Serial.println("− frite failed");
+    }
+}
+
+void appendFile(fs::FS &fs, const char *path, const char *message) {
+    Serial.printf("Appending to file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_APPEND);
+    if (!file) {
+        Serial.println("− failed to open file for appending");
+        return;
+    }
+    if (file.print(message)) {
+        Serial.println("− message appended");
+    } else {
+        Serial.println("− append failed");
+    }
+}
+
+void renameFile(fs::FS &fs, const char *path1, const char *path2) {
+    Serial.printf("Renaming file %s to %s\r\n", path1, path2);
+    if (fs.rename(path1, path2)) {
+        Serial.println("− file renamed");
+    } else {
+        Serial.println("− rename failed");
+    }
+}
+
+void deleteFile(fs::FS &fs, const char *path) {
+    Serial.printf("Deleting file: %s\r\n", path);
+    if (fs.remove(path)) {
+        Serial.println("− file deleted");
+    } else {
+        Serial.println("− delete failed");
+    }
+}
+
+void setup() {
+    Serial.begin(115200);
+    // put your setup code here, to run once:
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("Connecting...");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println(WiFi.localIP());
+
+    libssh_begin();
+
+    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+        Serial.println("SPIFFS Mount Failed");
+        return;
+    } else {
+        Serial.println("SPIFFS Mount Succeeded");
+    }
+
+    listDir(SPIFFS, "/", 0);
+    writeFile(SPIFFS, "/hello.txt", "Hello ");
+    appendFile(SPIFFS, "/hello.txt", "World!\r\n");
+    readFile(SPIFFS, "/hello.txt");
+    renameFile(SPIFFS, "/hello.txt", "/foo.txt");
+    readFile(SPIFFS, "/foo.txt");
+    deleteFile(SPIFFS, "/foo.txt");
+    Serial.println("Test complete");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+}
